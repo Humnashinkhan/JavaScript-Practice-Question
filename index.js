2ddd8934fddb6844be37ecd1ffec37cd7bdfa3ed
Humnashin Khan <humnashin9955@gmail.com>
2025-01-07 15:21:53 +0530

    practise question

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
 // let a = null
-// console.log(typeof null)
+// console.log(typeof null)    // object
+ 
+// console.log(a)  
+// var a = 5;  
+
+// const arr = [0,1,2,3]
+// arr[4] = 5
+// console.log(arr.length)
+
+// var x = 20;
+//  function foo() {
+//      console.log(x)
+//      var x =  10;
+//  }
+//   foo()
+
+// console.log('start')
+// setTimeout(() => {
+//     console.log('Timeout')
+// }, 2000)
+// console.log('ENd')
+
+// for(var i= 1; i<=3; i++){
+//     setTimeout(() => {
+//         console.log(i)
+//     }, 1000)
+// }
+
+// {
+//     let a = 1;
+//     let b = 2;
+//        console.log(a)
+//        console.log(b)
+// }
+// console.log(a)
+//        console.log(b)
+
+
+// const num1 = [1,2,3,4]
+// const num2 = [5,6,7,8]
+
+// const jointArray = [...num1 , ...num2]
+// console.log(jointArray)
+
+
