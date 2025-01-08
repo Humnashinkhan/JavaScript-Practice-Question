d33af583d6139f95531bb8fa11b1431bfd7474f2
Humnashin Khan <humnashin9955@gmail.com>
2025-01-08 22:53:18 +0530

    basic questions

--- index.js
+++ index.js
@@ -44,3 +44,40 @@
 // console.log(jointArray)
 
 
+// console.log ([1,2,3] == [1,2,3]);
+
+
+// console.log("5" + 3) it will concatenate two string output is 53 and it will convert the no int string(in case of Addition) 
+// console.log('5' - 3) behind the scene js is a typer convserion it automatically coverts
+// the string into a number(in case of Subtraction )
+
+
+// const numbers = [10,20,30,40,50]
+// const dividedByTwo = numbers.map(num => num / 2);
+
+// console.log(dividedByTwo);
+
+// console.log(1<2<3)
+// console.log(3>2>1)
+
+// console.log(5 == '5')
+// console.log(5 === '5')
+
+// var a = 5;
+// console.log(a++); post increment operator increse the value by 1 but give the privious value
+// console.log(a);
+
+// let a = null
+// console.log(typeof null);  object
+
+// const arr1 = [1,2]
+// const arr2 = [1,2]
+
+// console.log(arr1 == arr2);
+
+
+// const numbers = [0,1,2,3,4,5]
+// numbers.push(6)
+// console.log(numbers);
+
+
